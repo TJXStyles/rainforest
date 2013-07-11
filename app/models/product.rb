581257e411b178cc1a949b1924a8c91e6a1f5d71
Jack Cooc <jack.cooc@gmail.com>
2013-07-11 15:08:39 -0400

    Add image upload using paperclip and product edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price_in_cents
+  attr_accessible :description, :edit, :name, :price_in_cents, :image, :formatted_price
+  has_attached_file :image, :styles => { :medium => "300x300>" }
 
   has_many :reviews
   has_many :users, :through => :reviews
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => { :only_integer => true}
+  validates_attachment :image, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png']}, size: { less_than: 5.megabytes }
 
   def formatted_price
   	price_in_dollars = price_in_cents.to_f / 100
